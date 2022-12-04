5c81cf03befc2caa89c9ff9670961cc6eccb6553
kim-chimy <106795190+kim-chimy@users.noreply.github.com>
2022-12-04 18:17:04 +0700

    Initial Push

---
+++ DeployWinformWithSquirrel/WinformUI/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text += $" v{Application.ProductVersion}";
+            CheckForUpdate();
+        }
+
+        private async Task CheckForUpdate()
+        {
+            using (var manager = new UpdateManager(@"C:\Temp\Releases"))
+            {
+                await manager.UpdateApp();
+            }
+        }
+    }
+}
